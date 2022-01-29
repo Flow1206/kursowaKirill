fcde79c3420169f60021b55c413bd0a3c97e453d
Kirill <kirill.skoridinskiy@gmail.com>
2022-01-29 14:24:22 +0200

    dasdas

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Kirill")
+const TelegramApi  = require('node-telegram-bot-api')
+
+const request = require('request')
+
+const token = '5110146510:AAHLVJl-xIEIiZNG7cLKEN9eBZA0b_DsXvY'
+
+const bot = new TelegramApi(token, {polling: true})
+
+bot.onText(/\/start/, async (msg) =>{
+    console.log(msg)
+
+})
